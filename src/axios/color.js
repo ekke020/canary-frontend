a95ea5b458fdbb609d5f3fe2fee6a30574503689
Johan Ekman <johan.ekman@pricer.com>
2022-12-06 15:26:43 +0100

    Removed TCP

--- src/axios/color.js
+++ src/axios/color.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const getColor = async () => {
-  const response = await axios.get('tcp://10.44.10.79:8080');
+  const response = await axios.get('10.44.10.79:8080');
   return response.data.color;
 }
