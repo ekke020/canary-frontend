eaedd3a94c798b310e1fc258b2a21faa9da7cfa2
Johan Ekman <johan.ekman@pricer.com>
2022-12-06 16:15:04 +0100

    Logging

--- src/axios/color.js
+++ src/axios/color.js
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 export const getColor = async () => {
   const response = await axios.get('10.44.10.79:8080/color');
+  console.log(response);
   return response.data.color;
 }
