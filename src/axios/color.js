6ca994c465fe811a86d4cfa84eea521ef702e997
Johan Ekman <johan.ekman@pricer.com>
2022-12-06 15:20:55 +0100

    Testing backend access

--- src/axios/color.js
+++ src/axios/color.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const getColor = async () => {
-  const response = await axios.get('http://localhost:7878/color');
+  const response = await axios.get('tcp://10.44.10.79:8080');
   return response.data.color;
 }
