a1abf43e77099276d1f6d19b7220be6d91beecec
Johan Ekman <johan.ekman@pricer.com>
2022-12-06 16:42:57 +0100

    Testing

--- src/axios/color.js
+++ src/axios/color.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getColor = async () => {
-  const response = await axios.get('http://canary-backend-service:8080/color');
+  const response = await axios.get('canary-backend-service:8080/color');
   console.log(response);
   return response.data.color;
 }
